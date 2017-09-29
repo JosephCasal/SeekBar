940eef361a45a3dd17272173debf614b57f1a498
Joseph Casal <joseph@Josephs-MacBook-Air.local>
2017-09-29 12:45:51 -0400

    upload project

---
+++ app/src/main/java/com/example/joseph/seekbar/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.joseph.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private SeekBar sb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tvValue);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        int x = sb.getProgress();
+        tv.setText(Integer.toString(x));
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
